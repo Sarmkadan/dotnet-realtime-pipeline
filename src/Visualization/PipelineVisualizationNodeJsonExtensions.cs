afea570f9aeba45db9c196cc7f9b6a14c2e4f5be
Vladyslav Zaiets <rutova2@gmail.com>
2020-10-19 20:59:51 +0300

    Serialize PipelineVisualizationNodeJson properly

---
+++ src/Visualization/PipelineVisualizationNodeJsonExtensions.cs
@@ -0,0 +1,83 @@
+#nullable enable
+
+// =============================================================================
+// Author: Vladyslav Zaiets | https://sarmkadan.com
+// CTO & Software Architect
+// =============================================================================
+
+namespace DotNetRealtimePipeline.Visualization;
+
+using System;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+/// <summary>
+/// Provides JSON serialization and deserialization extensions for <see cref="PipelineVisualizationNode"/>.
+/// </summary>
+public static class PipelineVisualizationNodeJsonExtensions
+{
+    private static readonly JsonSerializerOptions _jsonOptions = new(JsonSerializerDefaults.Web)
+    {
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+        WriteIndented = false,
+        DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull
+    };
+
+    /// <summary>
+    /// Serializes the <see cref="PipelineVisualizationNode"/> to a JSON string.
+    /// </summary>
+    /// <param name="value">The node to serialize.</param>
+    /// <param name="indented">Whether to indent the JSON for readability.</param>
+    /// <returns>A JSON representation of the node.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="value"/> is null.</exception>
+    public static string ToJson(this PipelineVisualizationNode value, bool indented = false)
+    {
+        ArgumentNullException.ThrowIfNull(value);
+
+        var options = indented
+            ? new JsonSerializerOptions(_jsonOptions)
+            {
+                WriteIndented = true
+            }
+            : _jsonOptions;
+
+        return JsonSerializer.Serialize(value, options);
+    }
+
+    /// <summary>
+    /// Deserializes a <see cref="PipelineVisualizationNode"/> from a JSON string.
+    /// </summary>
+    /// <param name="json">The JSON string to deserialize.</param>
+    /// <returns>The deserialized node, or null if the JSON is invalid.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="json"/> is null.</exception>
+    public static PipelineVisualizationNode? FromJson(string json)
+    {
+        ArgumentNullException.ThrowIfNull(json);
+
+        return JsonSerializer.Deserialize<PipelineVisualizationNode>(json, _jsonOptions);
+    }
+
+    /// <summary>
+    /// Attempts to deserialize a <see cref="PipelineVisualizationNode"/> from a JSON string.
+    /// </summary>
+    /// <param name="json">The JSON string to deserialize.</param>
+    /// <param name="value">Receives the deserialized node, or null if deserialization fails.</param>
+    /// <returns>True if deserialization succeeded; otherwise, false.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="json"/> is null.</exception>
+    public static bool TryFromJson(string json, out PipelineVisualizationNode? value)
+    {
+        ArgumentNullException.ThrowIfNull(json);
+
+        try
+        {
+            value = JsonSerializer.Deserialize<PipelineVisualizationNode>(json, _jsonOptions);
+            return true;
+        }
+        catch (JsonException)
+        {
+            value = null;
+            return false;
+        }
+    }
+}
