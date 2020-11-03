f67b1cc8718077eae1e3c75759fc0bbe95175b95
Vladyslav Zaiets <rutova2@gmail.com>
2020-11-03 12:57:49 +0200

    Json serialization for CommandExecutorJson

---
+++ src/CLI/CommandExecutorJsonExtensions.cs
@@ -0,0 +1,79 @@
+#nullable enable
+
+namespace DotNetRealtimePipeline.CLI;
+
+using System;
+using System.Text.Json;
+
+/// <summary>
+/// Provides JSON serialization and deserialization extensions for <see cref="CommandExecutor"/>.
+/// </summary>
+public static class CommandExecutorJsonExtensions
+{
+    private static readonly JsonSerializerOptions _jsonOptions = new(JsonSerializerDefaults.Web)
+    {
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+        WriteIndented = false
+    };
+
+    /// <summary>
+    /// Serializes the <see cref="CommandExecutor"/> instance to a JSON string.
+    /// </summary>
+    /// <param name="value">The command executor instance to serialize.</param>
+    /// <param name="indented">Whether to format the JSON with indentation for readability.</param>
+    /// <returns>A JSON string representation of the command executor.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="value"/> is null.</exception>
+    public static string ToJson(this CommandExecutor value, bool indented = false)
+    {
+        ArgumentNullException.ThrowIfNull(value);
+
+        var options = indented
+            ? new JsonSerializerOptions(_jsonOptions) { WriteIndented = true }
+            : _jsonOptions;
+
+        return JsonSerializer.Serialize(value, options);
+    }
+
+    /// <summary>
+    /// Deserializes a JSON string to a <see cref="CommandExecutor"/> instance.
+    /// </summary>
+    /// <param name="json">The JSON string to deserialize.</param>
+    /// <returns>The deserialized command executor instance, or null if the JSON is invalid.</returns>
+    /// <exception cref="ArgumentException">Thrown when <paramref name="json"/> is null or blank.</exception>
+    public static CommandExecutor? FromJson(string json)
+    {
+        ArgumentException.ThrowIfNullOrWhiteSpace(json);
+
+        try
+        {
+            return JsonSerializer.Deserialize<CommandExecutor>(json, _jsonOptions);
+        }
+        catch (JsonException)
+        {
+            return null;
+        }
+    }
+
+    /// <summary>
+    /// Attempts to deserialize a JSON string to a <see cref="CommandExecutor"/> instance.
+    /// </summary>
+    /// <param name="json">The JSON string to deserialize.</param>
+    /// <param name="value">Receives the deserialized command executor instance if successful.</param>
+    /// <returns>True if deserialization succeeded; otherwise, false.</returns>
+    /// <exception cref="ArgumentException">Thrown when <paramref name="json"/> is null or blank.</exception>
+    public static bool TryFromJson(string json, out CommandExecutor? value)
+    {
+        ArgumentException.ThrowIfNullOrWhiteSpace(json);
+
+        try
+        {
+            value = JsonSerializer.Deserialize<CommandExecutor>(json, _jsonOptions);
+            return true;
+        }
+        catch (JsonException)
+        {
+            value = null;
+            return false;
+        }
+    }
+}
