0fb2a418e142736c74412ed4d26f2b3a6f88e55e
Vladyslav Zaiets <rutova2@gmail.com>
2020-11-18 18:20:06 +0200

    PipelineIntegrationTestsJson (de)serialization helpers

---
+++ tests/Integration/PipelineIntegrationTestsJsonExtensions.cs
@@ -0,0 +1,79 @@
+#nullable enable
+
+// =============================================================================
+// Author: Vladyslav Zaiets | https://sarmkadan.com
+// CTO & Software Architect
+// =============================================================================
+
+using System.Text.Json;
+using System.Text.Json.Serialization.Metadata;
+
+namespace DotNetRealtimePipeline.Tests.Integration;
+
+public static class PipelineIntegrationTestsJsonExtensions
+{
+    private static readonly JsonSerializerOptions _jsonOptions = new(JsonSerializerDefaults.Web)
+    {
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+        WriteIndented = false,
+        TypeInfoResolver = new DefaultJsonTypeInfoResolver(),
+        UnmappedMemberHandling = JsonUnmappedMemberHandling.Disallow,
+        UnknownTypeHandling = JsonUnknownTypeHandling.Disallow
+    };
+
+    /// <summary>
+    /// Serializes the <see cref="PipelineIntegrationTests"/> instance to a JSON string.
+    /// </summary>
+    /// <param name="value">The value to serialize.</param>
+    /// <param name="indented">Whether to indent the JSON for readability.</param>
+    /// <returns>A JSON string representation of the value.</returns>
+    /// <exception cref="ArgumentNullException">Thrown if <paramref name="value"/> is null.</exception>
+    public static string ToJson(this PipelineIntegrationTests value, bool indented = false)
+    {
+        ArgumentNullException.ThrowIfNull(value);
+
+        return JsonSerializer.Serialize(value, _jsonOptions with { WriteIndented = indented });
+    }
+
+    /// <summary>
+    /// Deserializes a JSON string to a <see cref="PipelineIntegrationTests"/> instance.
+    /// </summary>
+    /// <param name="json">The JSON string to deserialize.</param>
+    /// <returns>The deserialized <see cref="PipelineIntegrationTests"/> instance, or null if the JSON is empty.</returns>
+    /// <exception cref="ArgumentNullException">Thrown if <paramref name="json"/> is null.</exception>
+    /// <exception cref="JsonException">Thrown if the JSON is invalid or cannot be deserialized.</exception>
+    public static PipelineIntegrationTests? FromJson(string json)
+    {
+        ArgumentNullException.ThrowIfNull(json);
+
+        if (string.IsNullOrWhiteSpace(json))
+        {
+            return null;
+        }
+
+        return JsonSerializer.Deserialize<PipelineIntegrationTests>(json, _jsonOptions);
+    }
+
+    /// <summary>
+    /// Attempts to deserialize a JSON string to a <see cref="PipelineIntegrationTests"/> instance.
+    /// </summary>
+    /// <param name="json">The JSON string to deserialize.</param>
+    /// <param name="value">The deserialized value, or null if deserialization fails.</param>
+    /// <returns>True if deserialization succeeds; otherwise, false.</returns>
+    /// <exception cref="ArgumentNullException">Thrown if <paramref name="json"/> is null.</exception>
+    public static bool TryFromJson(string json, out PipelineIntegrationTests? value)
+    {
+        ArgumentNullException.ThrowIfNull(json);
+
+        try
+        {
+            value = JsonSerializer.Deserialize<PipelineIntegrationTests>(json, _jsonOptions);
+            return true;
+        }
+        catch (JsonException)
+        {
+            value = null;
+            return false;
+        }
+    }
+}
