3db76f6c144f1725593d057d36bfe31b4e831bb9
Vladyslav Zaiets <rutova2@gmail.com>
2020-11-20 06:18:16 +0200

    .
    will circle back to tests later.

---
+++ tests/dotnet-realtime-pipeline.Tests/Unit/DataPointTestsJsonExtensions.cs
@@ -0,0 +1,86 @@
+#nullable enable
+// =============================================================================
+// Author: Vladyslav Zaiets | https://sarmkadan.com
+// CTO & Software Architect
+// =============================================================================
+
+using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace DotNetRealtimePipeline.Tests.Unit;
+
+/// <summary>
+/// Provides System.Text.Json serialization helpers for <see cref="DataPointTests"/> instances.
+/// </summary>
+public static class DataPointTestsJsonExtensions
+{
+    private static readonly JsonSerializerOptions _jsonOptions = new(JsonSerializerDefaults.Web)
+    {
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+        WriteIndented = false,
+        DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull,
+        Converters = { new JsonStringEnumConverter(JsonNamingPolicy.CamelCase) }
+    };
+
+    /// <summary>
+    /// Serializes a <see cref="DataPointTests"/> instance to a JSON string.
+    /// </summary>
+    /// <param name="value">The data point tests instance to serialize.</param>
+    /// <param name="indented">Whether to format the JSON with indentation for readability.</param>
+    /// <returns>A JSON string representation of the data point tests instance.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="value"/> is null.</exception>
+    public static string ToJson(this DataPointTests value, bool indented = false)
+    {
+        ArgumentNullException.ThrowIfNull(value);
+
+        var options = indented
+            ? new JsonSerializerOptions(_jsonOptions) { WriteIndented = true }
+            : _jsonOptions;
+
+        return JsonSerializer.Serialize(value, options);
+    }
+
+    /// <summary>
+    /// Deserializes a JSON string to a <see cref="DataPointTests"/> instance.
+    /// </summary>
+    /// <param name="json">The JSON string to deserialize.</param>
+    /// <returns>A <see cref="DataPointTests"/> instance, or null if the JSON is empty or whitespace.</returns>
+    /// <exception cref="ArgumentException"><paramref name="json"/> is null or empty.</exception>
+    public static DataPointTests? FromJson(string json)
+    {
+        ArgumentException.ThrowIfNullOrEmpty(json);
+
+        try
+        {
+            return JsonSerializer.Deserialize<DataPointTests>(json, _jsonOptions);
+        }
+        catch (JsonException)
+        {
+            return null;
+        }
+    }
+
+    /// <summary>
+    /// Attempts to deserialize a JSON string to a <see cref="DataPointTests"/> instance.
+    /// </summary>
+    /// <param name="json">The JSON string to deserialize.</param>
+    /// <param name="value">When this method returns, contains the deserialized data point tests instance if successful; otherwise, null.</param>
+    /// <returns>True if the deserialization succeeded; otherwise, false.</returns>
+    /// <exception cref="ArgumentException"><paramref name="json"/> is null or empty.</exception>
+    public static bool TryFromJson(string json, out DataPointTests? value)
+    {
+        ArgumentException.ThrowIfNullOrEmpty(json);
+
+        try
+        {
+            value = JsonSerializer.Deserialize<DataPointTests>(json, _jsonOptions);
+            return true;
+        }
+        catch (JsonException)
+        {
+            value = null;
+            return false;
+        }
+    }
+}
