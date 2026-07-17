124f6b6522eaeca9f95bbe774e284fa63b13b02b
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-17 21:30:00 +0300

    readability pass over SlidingWindowAggregatorJsonExtensions

--- src/Services/SlidingWindowAggregatorJsonExtensions.cs
+++ src/Services/SlidingWindowAggregatorJsonExtensions.cs
@@ -10,8 +10,12 @@ using System;
 using System.Text.Json;
 
 /// <summary>
-/// Provides JSON serialization helpers for <see cref="SlidingWindowAggregator"/>.
+/// Provides JSON serialization and deserialization helpers for <see cref="SlidingWindowAggregator"/>.
 /// </summary>
+/// <remarks>
+/// This static class contains extension methods for serializing <see cref="SlidingWindowAggregator"/> instances
+/// to JSON strings and deserializing them back, using camelCase property naming policy by default.
+/// </remarks>
 public static class SlidingWindowAggregatorJsonExtensions
 {
     private static readonly JsonSerializerOptions JsonSerializerOptions = new()
@@ -24,9 +28,9 @@ public static class SlidingWindowAggregatorJsonExtensions
     /// Serializes a <see cref="SlidingWindowAggregator"/> instance to JSON.
     /// </summary>
     /// <param name="value">The <see cref="SlidingWindowAggregator"/> instance to serialize.</param>
-    /// <param name="indented">If true, formats the JSON with indentation.</param>
+    /// <param name="indented"><see langword="true"/> to format the JSON with indentation; otherwise, produces compact JSON.</param>
     /// <returns>A JSON string representation of the <see cref="SlidingWindowAggregator"/>.</returns>
-    /// <exception cref="ArgumentNullException">Thrown when <paramref name="value"/> is null.</exception>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="value"/> is <see langword="null"/>.</exception>
     public static string ToJson(this SlidingWindowAggregator value, bool indented = false)
     {
         ArgumentNullException.ThrowIfNull(value);
@@ -40,8 +44,8 @@ public static class SlidingWindowAggregatorJsonExtensions
     /// Deserializes a <see cref="SlidingWindowAggregator"/> instance from a JSON string.
     /// </summary>
     /// <param name="json">The JSON string to deserialize.</param>
-    /// <returns>A <see cref="SlidingWindowAggregator"/> instance.</returns>
-    /// <exception cref="ArgumentNullException">Thrown when <paramref name="json"/> is null or empty.</exception>
+    /// <returns>A <see cref="SlidingWindowAggregator"/> instance or <see langword="null"/> if deserialization yields <see langword="null"/>.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="json"/> is <see langword="null"/> or empty.</exception>
     /// <exception cref="JsonException">Thrown when JSON parsing fails.</exception>
     public static SlidingWindowAggregator? FromJson(string json)
     {
@@ -50,11 +54,12 @@ public static class SlidingWindowAggregatorJsonExtensions
     }
 
     /// <summary>
-    /// Tries to deserialize a <see cref="SlidingWindowAggregator"/> instance from a JSON string.
+    /// Attempts to deserialize a <see cref="SlidingWindowAggregator"/> instance from a JSON string.
     /// </summary>
     /// <param name="json">The JSON string to deserialize.</param>
-    /// <param name="value">The deserialized <see cref="SlidingWindowAggregator"/> instance, or null.</param>
-    /// <returns>True if deserialization succeeded; otherwise, false.</returns>
+    /// <param name="value">When this method returns, contains the deserialized <see cref="SlidingWindowAggregator"/> instance,
+    /// or <see langword="null"/> if deserialization failed.</param>
+    /// <returns><see langword="true"/> if deserialization succeeded; otherwise, <see langword="false"/>.</returns>
     public static bool TryFromJson(string json, out SlidingWindowAggregator? value)
     {
         try
