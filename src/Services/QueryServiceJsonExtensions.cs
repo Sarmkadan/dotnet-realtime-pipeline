7b29c6a2a0bba6120963af612a942b1253a4ef6d
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-17 21:30:00 +0300

    tidy up QueryServiceJsonExtensions.

--- src/Services/QueryServiceJsonExtensions.cs
+++ src/Services/QueryServiceJsonExtensions.cs
@@ -13,6 +13,10 @@ using System.Text.Json.Serialization;
 /// <summary>
 /// Provides System.Text.Json serialization extensions for <see cref="QueryService"/>.
 /// </summary>
+/// <remarks>
+/// This static class contains extension methods for serializing <see cref="QueryService"/> instances
+/// to JSON strings using camelCase property naming policy by default.
+/// </remarks>
 public static class QueryServiceJsonExtensions
 {
     private static readonly JsonSerializerOptions _jsonSerializerOptions = new(JsonSerializerDefaults.Web)
@@ -24,71 +28,22 @@ public static class QueryServiceJsonExtensions
     };
 
     /// <summary>
-    /// Serializes the <see cref="QueryService"/> instance to a JSON string.
+    /// Serializes a <see cref="QueryService"/> instance to JSON.
     /// </summary>
-    /// <param name="value">The query service instance to serialize.</param>
-    /// <param name="indented">Whether to format the JSON with indentation.</param>
-    /// <returns>A JSON string representation of the query service.</returns>
-    /// <exception cref="ArgumentNullException">Thrown when <paramref name="value"/> is null.</exception>
+    /// <param name="value">The <see cref="QueryService"/> instance to serialize.</param>
+    /// <param name="indented"><see langword="true"/> to format the JSON with indentation; otherwise, produces compact JSON.</param>
+    /// <returns>A JSON string representation of the <see cref="QueryService"/>.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="value"/> is <see langword="null"/>.</exception>
     public static string ToJson(this QueryService value, bool indented = false)
     {
         ArgumentNullException.ThrowIfNull(value);
 
-        var options = indented
-            ? new JsonSerializerOptions(_jsonSerializerOptions)
-            {
-                WriteIndented = true
-            }
-            : _jsonSerializerOptions;
+        var options = new JsonSerializerOptions(_jsonSerializerOptions)
+        {
+            WriteIndented = indented
+        };
 
         return JsonSerializer.Serialize(value, options);
     }
 
-    /// <summary>
-    /// Deserializes a JSON string to a <see cref="QueryService"/> instance.
-    /// Note: QueryService is a service class with dependencies and cannot be directly deserialized.
-    /// This method returns null when the JSON represents a QueryService placeholder.
-    /// </summary>
-    /// <param name="json">The JSON string to deserialize.</param>
-    /// <returns>A <see cref="QueryService"/> instance if deserialization succeeds, or null.</returns>
-    /// <exception cref="ArgumentNullException">Thrown when <paramref name="json"/> is null.</exception>
-    public static QueryService? FromJson(string json)
-    {
-        ArgumentNullException.ThrowIfNull(json);
-
-        try
-        {
-            // QueryService cannot be directly deserialized as it requires constructor dependencies
-            // This method exists for API consistency with other JsonExtensions classes
-            // and returns null since QueryService is a service class
-            return null;
-        }
-        catch (JsonException)
-        {
-            return null;
-        }
-    }
-
-    /// <summary>
-    /// Attempts to deserialize a JSON string to a <see cref="QueryService"/> instance.
-    /// </summary>
-    /// <param name="json">The JSON string to deserialize.</param>
-    /// <param name="value">Receives the deserialized instance, or null if deserialization fails.</param>
-    /// <returns>True if deserialization succeeds; otherwise, false.</returns>
-    /// <exception cref="ArgumentNullException">Thrown when <paramref name="json"/> is null.</exception>
-    public static bool TryFromJson(string json, out QueryService? value)
-    {
-        ArgumentNullException.ThrowIfNull(json);
-
-        try
-        {
-            value = null;
-            return false;
-        }
-        catch (JsonException)
-        {
-            value = null;
-            return false;
-        }
-    }
 }
