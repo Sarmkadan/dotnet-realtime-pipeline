f7f2bba38efcf5f1fc109ad2ef57f4a7eaff6e5e
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-17 21:30:00 +0300

    tighten guard clauses in RateLimitingMiddlewareJsonExtensions

--- src/Middleware/RateLimitingMiddlewareJsonExtensions.cs
+++ src/Middleware/RateLimitingMiddlewareJsonExtensions.cs
@@ -2,7 +2,7 @@
 // =============================================================================
 // Author: Vladyslav Zaiets | https://sarmkadan.com
 // CTO & Software Architect
-// =============================================================================
+// =====================================================================
 
 namespace DotNetRealtimePipeline.Middleware;
 
@@ -27,27 +27,18 @@ public static class RateLimitingMiddlewareJsonExtensions
     /// <param name="indented">If true, formats the JSON with indentation.</param>
     /// <returns>A JSON string representation of the <see cref="RateLimitingMiddleware"/>.</returns>
     /// <exception cref="ArgumentNullException">Thrown when <paramref name="value"/> is null.</exception>
-    public static string ToJson(this RateLimitingMiddleware value, bool indented = false)
-    {
-        ArgumentNullException.ThrowIfNull(value);
-
-        var options = new JsonSerializerOptions(_jsonSerializerOptions);
-        options.WriteIndented = indented;
-        return JsonSerializer.Serialize(value, options);
-    }
+    public static string ToJson(this RateLimitingMiddleware value, bool indented = false) =>
+        JsonSerializer.Serialize(value, new JsonSerializerOptions(_jsonSerializerOptions) { WriteIndented = indented });
 
     /// <summary>
     /// Deserializes a <see cref="RateLimitingMiddleware"/> instance from a JSON string.
     /// </summary>
     /// <param name="json">The JSON string to deserialize.</param>
     /// <returns>A <see cref="RateLimitingMiddleware"/> instance.</returns>
-    /// <exception cref="ArgumentNullException">Thrown when <paramref name="json"/> is null or empty.</exception>
+    /// <exception cref="ArgumentException">Thrown when <paramref name="json"/> is null or empty.</exception>
     /// <exception cref="JsonException">Thrown when JSON parsing fails.</exception>
-    public static RateLimitingMiddleware? FromJson(string json)
-    {
-        ArgumentNullException.ThrowIfNullOrEmpty(json);
-        return JsonSerializer.Deserialize<RateLimitingMiddleware>(json, _jsonSerializerOptions);
-    }
+    public static RateLimitingMiddleware? FromJson(string json) =>
+        JsonSerializer.Deserialize<RateLimitingMiddleware>(json, _jsonSerializerOptions);
 
     /// <summary>
     /// Tries to deserialize a <see cref="RateLimitingMiddleware"/> instance from a JSON string.
@@ -55,8 +46,10 @@ public static class RateLimitingMiddlewareJsonExtensions
     /// <param name="json">The JSON string to deserialize.</param>
     /// <param name="value">The deserialized <see cref="RateLimitingMiddleware"/> instance, or null.</param>
     /// <returns>True if deserialization succeeded; otherwise, false.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="json"/> is null or empty.</exception>
     public static bool TryFromJson(string json, out RateLimitingMiddleware? value)
     {
+        ArgumentNullException.ThrowIfNullOrEmpty(json);
         try
         {
             value = FromJson(json);
