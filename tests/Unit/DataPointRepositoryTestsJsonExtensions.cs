cf685c1326e513ae91067829b7de487e6a0370e7
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-17 21:30:00 +0300

    Small refactor - DataPointRepositoryTestsJsonExtensions

--- tests/Unit/DataPointRepositoryTestsJsonExtensions.cs
+++ tests/Unit/DataPointRepositoryTestsJsonExtensions.cs
@@ -1,4 +1,5 @@
 #nullable enable
+
 // =============================================================================
 // Author: Vladyslav Zaiets | https://sarmkadan.com
 // CTO & Software Architect
@@ -17,12 +18,12 @@ public static class DataPointRepositoryTestsJsonExtensions
     };
 
     /// <summary>
-    /// Serializes a <see cref="DataPointRepositoryTests"/> instance to a JSON string.
+    /// Serializes a <see cref="DataPointRepositoryTests"/> instance to a JSON string using camelCase property naming.
     /// </summary>
-    /// <param name="value">The test instance to serialize.</param>
+    /// <param name="value">The test instance to serialize. Cannot be null.</param>
     /// <param name="indented">Whether to format the JSON with indentation for readability.</param>
     /// <returns>A JSON string representation of the test instance.</returns>
-    /// <exception cref="ArgumentNullException">Thrown when <paramref name="value"/> is null.</exception>
+    /// <exception cref="ArgumentNullException"><paramref name="value"/> is null.</exception>
     public static string ToJson(this DataPointRepositoryTests value, bool indented = false)
     {
         ArgumentNullException.ThrowIfNull(value);
@@ -33,9 +34,10 @@ public static class DataPointRepositoryTestsJsonExtensions
     /// <summary>
     /// Deserializes a JSON string to a <see cref="DataPointRepositoryTests"/> instance.
     /// </summary>
-    /// <param name="json">The JSON string to deserialize.</param>
-    /// <returns>The deserialized test instance, or null if the JSON is empty or whitespace.</returns>
-    /// <exception cref="JsonException">Thrown when the JSON is invalid or cannot be deserialized.</exception>
+    /// <param name="json">The JSON string to deserialize. Cannot be null or empty.</param>
+    /// <returns>The deserialized test instance.</returns>
+    /// <exception cref="ArgumentException"><paramref name="json"/> is null or empty.</exception>
+    /// <exception cref="JsonException">The JSON is invalid or cannot be deserialized.</exception>
     public static DataPointRepositoryTests? FromJson(string json)
     {
         ArgumentException.ThrowIfNullOrEmpty(json);
@@ -46,8 +48,8 @@ public static class DataPointRepositoryTestsJsonExtensions
     /// <summary>
     /// Attempts to deserialize a JSON string to a <see cref="DataPointRepositoryTests"/> instance.
     /// </summary>
-    /// <param name="json">The JSON string to deserialize.</param>
-    /// <param name="value">Receives the deserialized instance if successful.</param>
+    /// <param name="json">The JSON string to deserialize. Cannot be null or empty.</param>
+    /// <param name="value">Receives the deserialized instance if successful; otherwise, null.</param>
     /// <returns>True if deserialization succeeded; otherwise, false.</returns>
     public static bool TryFromJson(string json, out DataPointRepositoryTests? value)
     {
