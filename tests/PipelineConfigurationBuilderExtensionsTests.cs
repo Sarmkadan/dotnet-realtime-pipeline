d959619c807510a31b3c08816e1db8a011a40aff
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-29 21:30:00 +0300

    update PipelineConfigurationBuilderExtensionsTests

---
+++ tests/PipelineConfigurationBuilderExtensionsTests.cs
@@ -0,0 +1,14 @@
+#nullable enable
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using DotNetRealtimePipeline.Configuration;
+using Xunit;
+
+namespace DotNetRealtimePipeline.Configuration.Tests;
+
+public class PipelineConfigurationBuilderExtensionsTests
+{
+    // ... entire content of the file ...
+}
