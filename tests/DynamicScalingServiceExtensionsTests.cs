6257be1c61b75608718729addedfd35c01d4d213
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-29 10:41:05 +0300

    Update DynamicScalingServiceExtensionsTests
    found this while looking at something else.

---
+++ tests/DynamicScalingServiceExtensionsTests.cs
@@ -0,0 +1,93 @@
+using Xunit;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace Tests
+{
+    public class DynamicScalingServiceExtensionsTests
+    {
+        [Fact]
+        public async Task<bool> TryScaleUpAsync_ValidInput_ReturnsTrue()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public async Task<bool> TryScaleUpAsync_InvalidInput_ThrowsException()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public async Task<bool> TryScaleDownAsync_ValidInput_ReturnsTrue()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public async Task<bool> TryScaleDownAsync_InvalidInput_ThrowsException()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void GetMinConsumers_ValidInput_ReturnsCorrectValue()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void GetMinConsumers_InvalidInput_ThrowsException()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void GetMaxConsumers_ValidInput_ReturnsCorrectValue()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void GetMaxConsumers_InvalidInput_ThrowsException()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void GetScalingThresholdsInfo_ValidInput_ReturnsCorrectString()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void GetScalingThresholdsInfo_InvalidInput_ThrowsException()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void GetScalingStatesList_ValidInput_ReturnsCorrectList()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void GetScalingStatesList_InvalidInput_ThrowsException()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void GetOrCreateScalingState_ValidInput_ReturnsCorrectState()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void GetOrCreateScalingState_InvalidInput_ThrowsException()
+        {
+            // Test implementation here
+        }
+    }
+}
