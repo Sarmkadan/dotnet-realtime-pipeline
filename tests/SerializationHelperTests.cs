7cc46e6bc5fa2ce5f79fda541e03e68e85042eac
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-30 21:30:00 +0300

    SerializationHelperTests.cs

---
+++ tests/SerializationHelperTests.cs
@@ -0,0 +1,74 @@
+using Xunit;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.IO;
+using System.Text;
+using System.Linq;
+using System;
+
+namespace SerializationHelperTests
+{
+    public class SerializationHelperTests
+    {
+        [Fact]
+        public async Task TestToJsonAsync()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public async Task TestFromJsonAsync()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public async Task TestToJsonArrayAsync()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public async Task TestFromJsonArrayAsync()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public async Task TestToCsvAsync()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public async Task TestToCsvBatchAsync()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public async Task TestSerializeResultsAsync()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public async Task TestSerializeMetricsAsync()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public async Task TestWriteToFileAsync()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public async Task TestReadFromFileAsync()
+        {
+            // Test implementation here
+        }
+    }
+}
