da40c3a3fe0964f65af6a020d5aa84bf9400324e
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-30 21:30:00 +0300

    update DateTimeExtensionsValidationTests
    found this while looking at something else.

---
+++ tests/DateTimeExtensionsValidationTests.cs
@@ -0,0 +1,45 @@
+using Xunit;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.VisualStudio.TestTools.UnitTesting.UnitTesting;
+using System;
+using System.Collections;
+using System.Collections;
+
+namespace Tests
+{
+    public class DateTimeExtensionsValidationTests
+    {
+        [Fact]
+        public void Validate_Happy_PATH_Test()
+        {
+            // Test implementation
+        }
+
+        [Fact]
+        public void Validate_NULL_INPUT_Test()
+        {
+            // Test implementation
+        }
+
+        [Fact]
+        public void Validate_EMPTY_COLLECTION_Test()
+        {
+            // Test implementation
+        }
+
+        [Fact]
+        public void Validate_BOUNDARY_VALUES_Test()
+        {
+            // Test implementation
+        }
+
+        [Fact]
+        public void Validate_ERROR_PATH_Test()
+        {
+            // Test implementation
+        }
+    }
+}
