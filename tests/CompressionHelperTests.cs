1eb1f1b175561627a15a7c0c67667325500c25b5
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-30 21:30:00 +0300

    update CompressionHelperTests

---
+++ tests/CompressionHelperTests.cs
@@ -0,0 +1,66 @@
+using System;
+using System.IO;
+using System.IO.Compression;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Utilities.Tests
+{
+    public class CompressionHelperTests
+    {
+        [Fact]
+        public async Task CompressGzipTest()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public async Task DecompressGzipTest()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public async Task CompressDeflateTest()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public async Task DecompressDeflateTest()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public async Task CompressFileAsyncTest()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public async Task DecompressFileAsyncTest()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public async Task CalculateCompressionRatioTest()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public async Task AnalyzeCompressionTest()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public async Task CompareAlgorithmsTest()
+        {
+            // Test implementation here
+        }
+    }
+}
