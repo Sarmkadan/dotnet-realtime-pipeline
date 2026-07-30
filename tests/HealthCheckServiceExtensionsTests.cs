1f64b142614f1985a6cbb76a3aea0bcfe84e7310
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-30 16:27:38 +0300

    update HealthCheckServiceExtensionsTests

---
+++ tests/HealthCheckServiceExtensionsTests.cs
@@ -0,0 +1,13 @@
+// tests/HealthCheckServiceExtensionsTests.cs
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Xunit;
+using DotNetRealtimePipeline.Monitoring;
+
+namespace DotNetRealtimePipeline.Tests.Monitoring;
+
+public class HealthCheckServiceExtensionsTests
+{
+    // ... entire content of the file ...
+}
