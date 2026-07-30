f9d2d68d6d0a1ae5b8f678a0cca4ef2bdc6be97f
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-30 21:30:00 +0300

    chore(tests): tweak CompressionHelperValidationTests

---
+++ tests/CompressionHelperValidationTests.cs
@@ -0,0 +1,76 @@
+using Xunit;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using Newtonsoft.Json;
+using System.IO;
+using System.IO.Compression;
+using System;
+
+namespace Tests
+{
+    public class CompressionHelperValidationTests
+    {
+        [Fact]
+        public void Validate_Happy_PATH()
+        {
+            // Test implementation
+        }
+
+        [Fact]
+        public void IsValid_HAPPY_PATH()
+        {
+            // Test implementation
+        }
+
+        [Fact]
+        public void EnsureValid_HAPPY_PATH()
+        {
+            // Test implementation
+        }
+
+        [Fact]
+        public void ValidateForCompressGzip_HAPPY_PATH()
+        {
+            // Test implementation
+        }
+
+        [Fact]
+        public void ValidateForDecompressGzip_HAPPY_PATH()
+        {
+            // Test implementation
+        }
+
+        [Fact]
+        public void ValidateForCompressDeflate_HAPPY_PATH()
+        {
+            // Test implementation
+        }
+
+        [Fact]
+        public void ValidateForDecompressDeflate_HAPPY_PATH()
+        {
+            // Test implementation
+        }
+
+        [Fact]
+        public void ValidateForCompressFileAsync_HAPPY_PATH()
+        {
+            // Test implementation
+        }
+
+        [Fact]
+        public void ValidateForDecompressFileAsync_HAPPY_PATH()
+        {
+            // Test implementation
+        }
+
+        [Fact]
+        public void ValidateForCalculateCompressionRatio_HAPPY_PATH()
+        {
+            // Test implementation
+        }
+    }
+}
