82d1961f8821dffd92ee234fdbd2ff2f6db16ca3
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-31 12:23:43 +0300

    update PathHelperTests
    also touched a couple of call sites.

---
+++ tests/PathHelperTests.cs
@@ -0,0 +1,83 @@
+using Xunit;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System;
+using System.Collections;
+using System.IO.Path;
+using System.Runtime.InteropServices;
+using System.Runtime.CompilerServices;
+using System.Diagnostics;
+using System.Reflection;
+using System.Globalization;
+using System;
+
+namespace Tests
+{
+    public class PathHelperTests
+    {
+        [Fact]
+        public void OriginalPathTest()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void NormalizedPathTest()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void PathHelperTest()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void IsValidPathTest()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void NormalizeTest()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void CombinePathsTest()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void GetRelativePathTest()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void IsPathInDirectoryTest()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void SanitizeFilenameTest()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void GenerateUniqueFilenameTest()
+        {
+            // Test implementation here
+        }
+    }
+}
