17a17681eaa1eec56aa175c3685d059b841394eb
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-31 13:35:04 +0300

    BatchProcessorExtensionsTests changes

---
+++ tests/BatchProcessorExtensionsTests.cs
@@ -0,0 +1,60 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+using Moq;
+using System;
+
+namespace Tests
+{
+    public class BatchProcessorExtensionsTests
+    {
+        [Fact]
+        public async Task TestProcessAsync_ValidInput_ReturnsValidOutput()
+        {
+            // Test implementation
+        }
+
+        [Fact]
+        public async Task TestProcessAsync_InvalidInput_ThrowsException()
+        {
+            // Test implementation
+        }
+
+        [Fact]
+        public void TestBatchSelect_ValidInput_ReturnsValidOutput()
+        {
+            // Test implementation
+        }
+
+        [Fact]
+        public async Task TestProcessAsync_ValidInput_ReturnsValidAggregate()
+        {
+            // Test implementation
+        }
+
+        [Fact]
+        public void TestGetEstimatedProcessingTime_ValidInput_ReturnsValidOutput()
+        {
+            // Test implementation
+        }
+
+        [Fact]
+        public async Task TestProcessBatchAsync_ValidInput_ReturnsValidOutput()
+        {
+            // Test implementation
+        }
+
+        [Fact]
+        public void TestCreateBatches_ValidInput_ReturnsValidOutput()
+        {
+            // Test implementation
+        }
+
+        [Fact]
+        public void TestGetBatchStatistics_ValidInput_ReturnsValidOutput()
+        {
+            // Test implementation
+        }
+    }
+}
