9cceb9c5ec9d6b44ff34607c630ec663bee76ad6
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-31 11:29:14 +0300

    Tweak CompressionHelperExtensionsTests

---
+++ tests/CompressionHelperExtensionsTests.cs
@@ -0,0 +1,65 @@
+using Xunit;
+using System.IO;
+using System.IO.Compression;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
+using System.Net;
+using System.Net.Sockets;
+using System.Net.Security;
+using System.Reflection;
+using System.Runtime;
+using System;
+
+namespace DotnetRealtimePipeline.Tests
+{
+    public class CompressionHelperExtensionsTests
+    {
+        [Fact]
+        public void CompressFile_HappyPath_Test()
+        {
+            // Test implementation
+        }
+
+        [Fact]
+        public void CalculateCompressionRatio_HappyPath_Test()
+        {
+            // Test implementation
+        }
+
+        [Fact]
+        public void CompressStream_HappyPath_Test()
+        {
+            // Test implementation
+        }
+
+        [Fact]
+        public void CompressFile_NullInput_Test()
+        {
+            // Test implementation
+        }
+
+        [Fact]
+        public void CalculateCompressionRatio_EmptyCollection_Test()
+        {
+            // Test implementation
+        }
+
+        [Fact]
+        public void CompressStream_BoundaryValue_Test()
+        {
+            // Test implementation
+        }
+
+        [Fact]
+        public void CompressFile_ExpectedException_Test()
+        {
+            // Test implementation
+        }
+    }
+}
